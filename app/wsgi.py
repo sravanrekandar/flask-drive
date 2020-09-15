a1d8366a4d7dab5e7a74bfcdd3cdd5e6642da3a4
sravanrekandar <sravanrekandar@gmail.com>
2020-09-15 21:20:45 +0530

    Procfile experiments

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,5 +2,5 @@ import os
 from .run import app
 
 port = int(os.environ.get("PORT", 5000))
-
+print(f"------Starting app on port={port}")
 application = app.run(host="0.0.0.0", port=port, debug=True)
